c71f2ca046b4370c048e240844f4bc8d89a0d8a7
Alan Chu <alanhdchu@utexas.edu>
2024-09-04 23:08:32 -0500

    added ws fix http

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
-from fastapi import FastAPI
-from .routers import refresh_database, users, courses, submissions
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.include_router(refresh_database.router)
-app.include_router(users.router)
-# app.include_router(courses.router)
-# app.include_router(submissions.router)
+# 設置 CORS 規則
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允許所有來源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允許所有 HTTP 方法
+    allow_headers=["*"],  # 允許所有 HTTP 標頭
+)
 
 
+# WebSocket 连接
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        print(f"Message received: {data}")
+        await websocket.send_text(f"Message text was: {data}")
+
+
+# HTTP 根路由
 @app.get("/")
 def root():
     return {"message": "Welcome to the Music Learning Platform!"}
